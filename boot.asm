6f97a57e02f4540b18b7156a2089e2cac4f9e8d3
Zhigang Song <songzhigang.szg@bytedance.com>
2025-06-10 00:21:29 +0800

    init commit

---
+++ boot.asm
@@ -0,0 +1,14 @@
+mov ax, 0xb800
+mov ds, ax
+
+; 显式 asm
+mov byte [0x00], 'a'
+mov byte [0x01], 0x01
+mov byte [0x02], 's'
+mov byte [0x03], 0x74
+mov byte [0x04], 'm'
+mov byte [0x05], 0x04
+jmp $
+
+times 510 - ($-$$) db 0
+dw 0xaa55
