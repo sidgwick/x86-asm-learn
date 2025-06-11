1c721462ce1bddc560696a23ec683f42980eed62
Zhigang Song <songzhigang.szg@bytedance.com>
2025-06-11 13:28:25 +0800

    明确设置数据段

--- boot.asm
+++ boot.asm
@@ -4,10 +4,10 @@
     mov ax, 0xb800
     mov es, ax
 
-    ; ; CS, DS 都设置为从 0x7c00 开始
-    ; mov ax, 0x7c00
-    ; mov cs, ax
-    ; mov ds, ax
+    ; CS 会被 BIOS 设置为 0x00, IP 被设置为 0x7c00
+    ; DS 设置为从 0x7c00 开始
+    mov ax, 0x7c00
+    mov ds, ax
 
 
     ; 以下显示字符串"Label offset:"
@@ -41,60 +41,56 @@
     ; 取得标号number的偏移地址
     mov ax, number
 
-    ; 设置数据段的基地址和代码段一样
-    mov cx, cs
-    mov ds, cx
-
     ; bx 存放的是除数
     mov bx, 10
 
     ; 求个位上的数字, DX 存放的是被除数高位, AX 存放的是被除数低位
     mov dx, 0
     div bx
-    mov [0x7c00 + number + 0x00], dl ; 保存个位数字
+    mov [number + 0x00], dl ; 保存个位数字
 
     ; 求十位数字, 第一次出发完成之后, DX 存放的是商, AX 是余数
     xor dx, dx
     div bx
-    mov [0x7c00 + number + 0x01], dl
+    mov [number + 0x01], dl
 
     ; 求百位数字
     xor dx, dx
     div bx
-    mov [0x7c00 + number + 0x02], dl
+    mov [number + 0x02], dl
 
     ; 求千位位数字
     xor dx, dx
     div bx
-    mov [0x7c00 + number + 0x03], dl
+    mov [number + 0x03], dl
 
     ; 求万位位数字
     xor dx, dx
     div bx
-    mov [0x7c00 + number + 0x04], dl
+    mov [number + 0x04], dl
 
     ; 现在显示计算好的数字到屏幕上
-    mov al, [0x7c00 + number + 0x04]
+    mov al, [number + 0x04]
     add al, 0x30 ; 转换为 ASCII 码
     mov [es:0x1a], al
     mov byte [es:0x1b], 0x04 ; 黑底红字
 
-    mov al, [0x7c00 + number + 0x03]
+    mov al, [number + 0x03]
     add al, 0x30 ; 转换为 ASCII 码
     mov [es:0x1c], al
     mov byte [es:0x1d], 0x04 ; 黑底红字
 
-    mov al, [0x7c00 + number + 0x02]
+    mov al, [number + 0x02]
     add al, 0x30 ; 转换为 ASCII 码
     mov [es:0x1e], al
     mov byte [es:0x1f], 0x04 ; 黑底红字
 
-    mov al, [0x7c00 + number + 0x01]
+    mov al, [number + 0x01]
     add al, 0x30 ; 转换为 ASCII 码
     mov [es:0x20], al
     mov byte [es:0x21], 0x04 ; 黑底红字
 
-    mov al, [0x7c00 + number + 0x00]
+    mov al, [number + 0x00]
     add al, 0x30 ; 转换为 ASCII 码
     mov [es:0x22], al
     mov byte [es:0x23], 0x04 ; 黑底红字
@@ -109,5 +105,5 @@ infi:
 number:
     db 0, 0, 0, 0, 0
 
-    times 203 db 0
+    times 202 db 0
     db 0x55, 0xaa
