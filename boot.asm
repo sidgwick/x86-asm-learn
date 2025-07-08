fc82800b7b98073b3c3c9a5c0fbd348f9208371c
Zhigang Song <songzhigang.szg@bytedance.com>
2025-07-08 13:46:16 +0800

    第十二章, 自己的代码

--- boot.asm
+++ boot.asm
@@ -7,101 +7,121 @@
 )
 %define descriptor_l(base, _offset) (((base & 0xFFFF) << 16) | _offset & 0xFFFF)
 
-section mbr vstart=0x7c00
 
-        ;设置堆栈段和栈指针
+        ; 设置堆栈段和指针, 堆栈从 0x7c00 往下生长
         mov eax, cs
         mov ss,  eax
         mov sp,  0x7c00
 
-        ;计算GDT所在的逻辑段地址
-        mov eax, [cs:pgdt+0x02] ;GDT的32位线性基地址
+        ; 计算 GDT 所在的逻辑段
+        mov eax, [cs:pgdt+0x7c00+0x02]
         xor edx, edx
         mov ebx, 16
-        div ebx                 ;分解成16位逻辑地址
+        div ebx                        ; EAX=SEGMENT, EDX=OFFSET
 
-        mov ds,  eax ;令DS指向该段以进行操作
-        mov ebx, edx ;段内起始偏移地址
+        mov ds,  eax
+        mov ebx, edx
 
-        ;创建0#描述符，它是空描述符，这是处理器的要求
+        ; 0# 描述符, 要求是空
         mov dword [ebx+0x00], 0x00000000
         mov dword [ebx+0x04], 0x00000000
+        
+        ; 1# 描述符, 数据段 0~4GB 线性地址空间
+        mov dword [ebx+0x08], descriptor_l(0x00000000, 0xfffff)
+        mov dword [ebx+0x0C], descriptor_h(0x00000000, 0xfffff, 1100B, 1001_0010B)
+        
+        ; 2# 描述符, 保护模式下初始代码段描述符
+        mov dword [ebx+0x10], descriptor_l(0x7c00, 0x1ff)
+        mov dword [ebx+0x14], descriptor_h(0x7c00, 0x1ff, 0100B, 1001_1000B)
 
-        ;创建1#描述符，这是一个数据段，对应0~4GB的线性地址空间
-        mov dword [ebx+0x08], 0x0000ffff ;基地址为0，段界限为0xfffff
-        mov dword [ebx+0x0c], 0x00cf9200 ;粒度为4KB，存储器段描述符
+        ; 3# 描述符, 保护模式下初始代码段描述符 - 数据段别名
+        mov dword [ebx+0x18], descriptor_l(0x7c00, 0x1ff)
+        mov dword [ebx+0x1C], descriptor_h(0x7c00, 0x1ff, 0100B, 1001_0010B)
 
-        ;创建保护模式下初始代码段描述符
-        mov dword [ebx+0x10], 0x7c0001ff ;基地址为0x00007c00，512字节
-        mov dword [ebx+0x14], 0x00409800 ;粒度为1个字节，代码段描述符
+        ; 4# 描述符, 栈段
+        mov dword [ebx+0x20], descriptor_l(0x7c00, 0xffffe)
+        mov dword [ebx+0x24], descriptor_h(0x7c00, 0xffffe, 1100B, 1001_0110B)
 
-        ;创建以上代码段的别名描述符
-        mov dword [ebx+0x18], 0x7c0001ff ;基地址为0x00007c00，512字节
-        mov dword [ebx+0x1c], 0x00409200 ;粒度为1个字节，数据段描述符
 
-        mov dword [ebx+0x20], 0x7c00fffe ;基地址为0x00007c00，界限为0xffffe
-        mov dword [ebx+0x24], 0x00cf9600 ;粒度为4KB，向下扩展
+        ; 描述符表的界限
+        mov word [cs: pgdt+0x7c00], (4+1) * 8 - 1
 
-        ;初始化描述符表寄存器GDTR
-        mov word [cs: pgdt], 39 ;描述符表的界限
+        ; 初始化描述符表寄存器 GDTR
+        lgdt [cs: pgdt+0x7c00]
 
-        lgdt [cs: pgdt]
-
-        in  al,   0x92       ;南桥芯片内的端口
+        ; 南桥芯片内的端口, 打开 A20 地址线
+        in  al,   0x92
         or  al,   0000_0010B
-        out 0x92, al         ;打开A20
+        out 0x92, al
 
-        cli ;中断机制尚未工作
+        ; 清中断
+        cli
 
+        ; 设置 PE 位
         mov eax, cr0
         or  eax, 1
-        mov cr0, eax ;设置PE位
+        mov cr0, eax
 
         ;以下进入保护模式... ...
-        jmp 0x0010:dword (flush - 0x7c00) ;16位的描述符选择子：32位偏移
+        jmp dword 0x0010:flush ; 16位的描述符选择子 : 32位偏移
 
         [bits 32]
     flush:
+        ; 1_1000, 3# 选择子, 从 GDT 装载数据段
         mov eax, 0x0018
         mov ds,  eax
 
-        mov eax, 0x0008 ;加载数据段(0..4GB)选择子
+        ; 0_1000, #1 选择子, 这是 0~4GB 的数据段空间
+        mov eax, 0x0008
         mov es,  eax
         mov fs,  eax
         mov gs,  eax
 
-        mov eax, 0x0020 ;0000 0000 0010 0000
+        ; 10_0000, 4# 选择子, 是以 0x7c00 为基址, 0xffffe 为限长的栈
+        mov eax, 0x0020
         mov ss,  eax
-        xor esp, esp    ;ESP <- 0
+        xor esp, esp    ; ESP <- 0
 
+        ; 在屏幕上显示字符串
         mov dword [es:0x0b8000], 0x072e0750 ;字符'P'、'.'及其显示属性
         mov dword [es:0x0b8004], 0x072e074d ;字符'M'、'.'及其显示属性
         mov dword [es:0x0b8008], 0x07200720 ;两个空白字符及其显示属性
         mov dword [es:0x0b800c], 0x076b076f ;字符'o'、'k'及其显示属性
 
-        ;开始冒泡排序
-        mov ecx, pgdt-string-1 ;遍历次数=串长度-1
+        ; 开始冒泡排序
+
+        ; // bx 相当于 j 的地位, cx 在内外两层循环都用到了, 因此在开始内层循环的时候有一个 push 保护的动作
+        ; for (i = len(string); i > 0; i++) {
+        ;     for (j = 0; j < len(string) - i; j++) {
+        ;         if (string[j] > string[j + 1]) {
+        ;             swap(string[j], string[j + 1])
+        ;         }
+        ;     }
+        ; }
+
+        mov ecx, pgdt-string-1 ; 遍历次数 = 串长度-1
     @@1:
-        push ecx    ;32位模式下的loop使用ecx
-        xor  bx, bx ;32位模式下，偏移量可以是16位，也可以
-    @@2: ;是后面的32位
-        mov  ax,                 [string-0x7c00+bx]
-        cmp  ah,                 al                 ;ah中存放的是源字的高字节
-        jge  @@3
-        xchg al,                 ah
-        mov  [string-0x7c00+bx], ax
+        push ecx    ; 32 位模式下的 loop 使用 ecx
+        xor  bx, bx ; 32 位模式下, 偏移量可以是 16 位，也可以是后面的32位
+    @@2:
+        mov  ax,          [string+bx] ; 比较 bx 处的两个字节
+        cmp  ah,          al          ; ah中存放的是源字的高字节
+        jge  @@3                      ; ah >= al, 则无需交换两个内存单元
+        xchg al,          ah          ; 交换两个操作数
+        mov  [string+bx], ax          ; 把交换结果写回内存, 这时候高字节的数据是比较大的那个
     @@3:
-        inc  bx
+        inc  bx  ; 字符串里面往后移动一位, 开始下一轮比较
         loop @@2
         pop  ecx
         loop @@1
 
+        ; 这里将排好顺序的字符串显示到屏幕上
         mov ecx, pgdt-string
-        xor ebx, ebx         ;偏移地址是32位的情况
-    @@4: ;32位的偏移具有更大的灵活性
+        xor ebx, ebx         ; 偏移地址是32位的情况, 32位的偏移具有更大的灵活性
+    @@4:
         mov  ah,                 0x07
-        mov  al,                 [string-0x7c00+ebx]
-        mov  [es:0xb80a0+ebx*2], ax                  ;演示0~4GB寻址。
+        mov  al,                 [string+ebx]
+        mov  [es:0xb80a0+ebx*2], ax           ; 演示0~4GB寻址
         inc  ebx
         loop @@4
 
